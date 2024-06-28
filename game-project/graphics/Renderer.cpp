28fd9e8fb0f96fafe84af83c52e1f94fc44157bf
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-28 17:29:12 +0530

    Added: `game-project/graphics/Renderer.cpp`

---
+++ game-project/graphics/Renderer.cpp
@@ -0,0 +1,46 @@
+#include "Renderer.h"
+
+Renderer::Renderer()
+{
+    // Initialize renderer
+}
+
+Renderer::~Renderer()
+{
+    // Destroy renderer
+}
+
+void Renderer::renderPlayer(Player* player)
+{
+    // Render player on screen
+}
+
+void Renderer::renderObstacle(Obstacle* obstacle)
+{
+    // Render obstacle on screen
+}
+
+void Renderer::renderCollectible(Collectible* collectible)
+{
+    // Render collectible on screen
+}
+
+void Renderer::renderEnemy(Enemy* enemy)
+{
+    // Render enemy on screen
+}
+
+void Renderer::renderLevel(Level* level)
+{
+    // Render level on screen
+}
+
+void Renderer::renderGameOverScreen()
+{
+    // Render game over screen
+}
+
+void Renderer::renderHUD(int score, int lives)
+{
+    // Render HUD elements (score, lives, etc.)
+}
